ce80bbb8c1df8ea05b92ba5c98ed3347aa9a46fc
aoudija <amineoudija02@yahoo.com>
2022-12-04 08:25:21 +0100

    missclick

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -6,11 +6,14 @@
 /*   By: aoudija <aoudija@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/02 20:04:34 by aoudija           #+#    #+#             */
-/*   Updated: 2022/12/02 20:52:35 by aoudija          ###   ########.fr       */
+/*   Updated: 2022/12/04 08:21:00 by aoudija          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line_bonus.h"
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
 
 size_t	ft_strlen(const char *str)
 {
@@ -91,10 +94,10 @@ char	*ft_line(char **str, int fd)
 	return (line);
 }
 
-char	*get_next_line_bonus(int fd)
+char	*get_next_line(int fd)
 {
 	char		*buffer;
-	static char	**t;
+	static char	*t[1024];
 	char		*line;
 
 	buffer = malloc(BUFFER_SIZE + 1);
@@ -107,21 +110,78 @@ char	*get_next_line_bonus(int fd)
 	return (line);
 }
 
+char *add_char(char *s)
+{
+	char *p = calloc(strlen(s) + 1, 1);
+	int  i = -1;
+	while (s[++i])
+		p[i] = s[i];
+	p[i++] = 0;
+	free(s);
+	return p;
+}
+
 int main()
 {
-	int fd = open("text.txt",O_RDWR);
-	int fd2 = open("t2.txt",O_RDWR);
-	printf("%d\n%d\n", fd, fd2);
-	char *s;
-	// s = get_next_line_bonus(fd);
-	// while (s)
+	char *p = ft_strdup("1");
+
+	char *s = ft_strdup("1");
+	int j = 0;
+	int  k = 1;
+	int fd[OPEN_MAX + 1];
+	while (j < OPEN_MAX)
+	{
+		fopen(s, "w+");
+		fd[j] = open(s, O_RDWR);
+		write(fd[j], p, strlen(p));
+		s = add_char(ft_itoa(k));
+		p = add_char(p);
+		if (k == 1)
+			p = add_char(p);			
+		// if (k == 10)
+		// 	k = 0;
+		k++;
+		j++;
+	}
+	
+	// free(s);
+	j = 0;
+	k = 1;
+	s = ft_strdup("1");
+	while(j < 9000)
+	{
+		fd[j] = open(s, O_RDWR);
+		char *d = get_next_line(fd[j]);
+		while (d)
+		{
+		printf("%s\n", d);
+		free(d);
+		d = get_next_line(fd[j]);
+		}
+		printf("--------------------\n");
+		j++;
+		s = add_char(ft_itoa(k));
+		// if (k == 10)
+		// 	k = 0;
+		k++;
+		usleep(10000);
+	}
+	// int fd = open("text.txt",O_RDWR);
+	// // int fd2 = open("t2.txt",O_RDWR);
+	// char *s;
+	// char *t;
+	// // s = get_next_line_bonus(fd2); 
+	// t = get_next_line_bonus(fd); 
+	// int i = 0;
+	// while (i < 1000)
 	// {
-		printf("%s", get_next_line_bonus(fd));
-	// 	free(s);
-	// 	s = get_next_line_bonus(fd);
+	// 	open("t")
 	// }
-	// free(s);
-	// printf("%s",s);
-	// free(s);
-	// while (1);
+	// while (s && t)
+	// {
+	// 	printf("%s",s);
+	// 	printf("%s",t);
+	// 	s = get_next_line_bonus(fd2);
+	// 	t = get_next_line_bonus(fd);
+	// }
 }
--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -6,7 +6,7 @@
 /*   By: aoudija <aoudija@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/02 20:25:44 by aoudija           #+#    #+#             */
-/*   Updated: 2022/12/02 20:32:53 by aoudija          ###   ########.fr       */
+/*   Updated: 2022/12/04 08:12:39 by aoudija          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@
 # include <stdio.h>
 # include <fcntl.h>
 
-char	*get_next_line_bonus(int fd);
+char	*get_next_line(int fd);
 size_t	ft_strlen(const char *str);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	ft_strlcat(char *dst, const char *src, size_t n);
@@ -26,6 +26,7 @@ char	*ft_strdup(char *s1);
 char	*ft_strjoin(char *s1, char *s2);
 char	*ft_substr(char *s, unsigned int start, size_t len);
 char	*ft_strchr(char *s, int c);
+char	*ft_itoa(int n);
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 42
